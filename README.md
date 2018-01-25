844f7d96d54675340517618705b228d55b24e333
Brian Mendez <brimende1@cobogpgp5651.global.publicisgroupe.net>
2018-01-25 12:11:39 -0500

    js and css code success

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-comentar js
-preparar pitch enfocado a buenas prácticas en FE y gulp
-
 # link-slider project guideline
 
 ## Description
@@ -10,13 +7,13 @@ Simple slider that runs up to 3 sliders, this is a work in progress, for more in
 
 ## How to Run
 
-Go to `src/` then run `gulp`
+Go to `src/` then run `gulp`.
 
 ## Project Workflow
 
-All the development files are located in `src/` folder, please do not modify  any *css* of *js* from the root folders, it will be overwritten.
+All the development files are located in `src/` folder, please do not modify  any *css* or *js* from the root folders, it will be overwritten.
 
-> When committing, a wild pre-commit will spawn and will check all your *js*, *sass* code quality, so you may need to install *jshint* client `npm install -g jshint`
+> When committing, a wild pre-commit will spawn and will check all your *js*, *sass* code quality, so you may need to install *jshint* client `npm install -g jshint`.
 
 <p id="todos"></p>
 ## TODO'S
--- src/js/main.js
+++ src/js/main.js
@@ -11,23 +11,25 @@ function goToSlide(n) {
   var nextSlide;
 // Instance the current slide
   currentSlide = n;
-
+/// Searching current slide value
   if (currentSlide > slides.length - 1) {
     currentSlide = 0;
   } else if (currentSlide < 0) {
     currentSlide = slides.length - 1;
   }
-
+// removing slider's class
   for (var i = 0; i < slides.length; i++) {
     slides[i].classList.remove('showing', 'first', 'last');
   }
-
+// adding 'showing' class for the current slide
   slides[currentSlide].classList.add('showing');
-
+// if currentSlide was greater than "0", subtract "1"
   previousSlide = (currentSlide > 0) ? currentSlide - 1 : slides.length - 1;
+// if currentSlide was less than "0", add "1"
   nextSlide = (currentSlide < slides.length - 1) ? currentSlide + 1 : 0;
-
+//currentSlide become previousSlide, add 'first' class.
   slides[previousSlide].classList.add('first');
+//currentSlide become nextSlide, add 'last' class.
   slides[nextSlide].classList.add('last');
 }
 
